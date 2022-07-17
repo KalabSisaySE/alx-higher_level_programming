7638c732913e061fb2838955cf5943c29fc83867
KalabSisaySE <kyimusic11@gmail.com>
2022-07-17 08:37:55 -0700

    complete task-0 'Positive anything is better than negative nothing'

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+if number < 0:
+    type = "negative"
+elif number == 0:
+    type = "zero"
+else :
+    type = "positive"
+    
+print(f"{number} is {type}")
