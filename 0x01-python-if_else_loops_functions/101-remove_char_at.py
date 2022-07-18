6486170f3cc3d354fc3ac66c22f5dc902f1e1cb2
KalabSisaySE <kyimusic11@gmail.com>
2022-07-18 15:08:45 -0700

    complete task-15

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    new_str = ""
+    i = 0
+    for ch in str:
+        if(i == n):
+            ch = ""
+        new_str += ch
+        i += 1
+    return new_str
