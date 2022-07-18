73f0385d2931e3a231193a3966506d0bf59ecde3
KalabSisaySE <kyimusic11@gmail.com>
2022-07-18 14:13:15 -0700

    fix task-8 endline

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,6 +2,6 @@
 def uppercase(str):
     for ch in str:
         if (ord(ch) >= 97) and (ord(ch) <= 122):
-            print("{}".format(chr(ord(ch) - 32)), end="")
-        else:
-            print("{}".format(ch), end="")
+            ch = chr(ord(ch) - 32)
+        print("{}".format(ch), end="")
+    print("")
