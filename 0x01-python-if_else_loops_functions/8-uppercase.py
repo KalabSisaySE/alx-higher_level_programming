7c5bec5712101f8521e8cee6a0533c69ab33da81
KalabSisaySE <kyimusic11@gmail.com>
2022-07-18 13:00:16 -0700

    complete task-8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3         
+def uppercase(str):
+    for ch in str:
+        if (ord(ch) >= 97) and (ord(ch) <= 122):
+          print("{}".format(chr(ord(ch) - 32)), end="")
+        else:
+            print("{}".format(ch), end="")
