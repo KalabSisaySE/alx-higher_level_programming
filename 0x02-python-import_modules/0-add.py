e71051d31ca845ebc4aa52d132a0022c585741ad
KalabSisaySE <kyimusic11@gmail.com>
2022-07-20 13:54:18 -0700

    fix task-0 prevent running when imported

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,4 +2,5 @@
 import add_0 as addition
 a = 1
 b = 2
-print("{} + {} = {}".format(a, b, addition.add(a, b)))
+if __name__ == "__main__":
+    print("{} + {} = {}".format(a, b, addition.add(a, b)))
