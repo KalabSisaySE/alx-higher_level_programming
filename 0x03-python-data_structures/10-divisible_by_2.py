55b0915649d0430fad5d7fedbbebf1d36f944b28
KalabSisaySE <kyimusic11@gmail.com>
2022-07-23 10:20:27 -0700

    complete task-10

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    bool_list = []
+    print(len(my_list))
+    for i in my_list:
+        if i % 2 == 0:
+            value = True
+        else:
+            value = False
+        bool_list.append(value)
+    return bool_list
