63e64f8bbcf2b66f156881c1220a557d9f65624f
KalabSisaySE <kyimusic11@gmail.com>
2022-07-24 08:39:24 -0700

    fix task-9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
-def multiple_returns(sentence):
-    print()
-    # tuple again do it later
+def max_integer(my_list=[]):
+    if len(my_list) < 1:
+        return None
+    else:
+        maximum = my_list[0]
+        for i in my_list:
+            if i > maximum:
+                maximum = i
+        return maximum
