035da9eb18c8d810042680f9925ff45907298e8d
KalabSisaySE <kyimusic11@gmail.com>
2022-07-24 10:41:45 -0700

    complete task-7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,15 +2,15 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     def add_sec_elm(my_tuple=()):
         if len(my_tuple) < 2:
-            if len(my_tuple) == 1: 
+            if len(my_tuple) == 1:
                 new_tuple = (my_tuple[0], 0)
                 return new_tuple
-            elif len(my_tuple) == 0: 
+            elif len(my_tuple) == 0:
                 new_tuple = (0, 0)
                 return new_tuple
         else:
             return my_tuple
-    new_tuple_a = add_sec_elm(tuple_a)
-    new_tuple_b = add_sec_elm(tuple_b)
-    add_tuple = (new_tuple_a[0] + new_tuple_b[0]), (new_tuple_a[1] + new_tuple_b[1])
+    a = add_sec_elm(tuple_a)
+    b = add_sec_elm(tuple_b)
+    add_tuple = (a[0] + b[0]), (a[1] + b[1])
     return add_tuple
