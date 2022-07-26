7edd232f954a08ad01efe32006d7c0e70f52f1e5
KalabSisaySE <kyimusic11@gmail.com>
2022-07-26 13:33:20 -0700

    complete task-8

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import sys
+def safe_function(fct, *args):
+    result = None
+    try:
+        return fct(*args)
+    except Exception as err:
+        sys.stderr.write("Exception: {}\n".format(err))
+        return result
