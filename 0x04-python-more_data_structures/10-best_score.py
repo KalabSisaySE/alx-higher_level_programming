35b33b036528aaa274f938b2b6ed3892514c2ca0
KalabSisaySE <kyimusic11@gmail.com>
2022-07-26 13:01:11 -0700

    fix task-10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary:
+    if type(a_dictionary) == dict:
         list_value = list(a_dictionary.values())
-        biggest = list_value[0]
-        for x in a_dictionary:
-            if a_dictionary[x] > biggest:
-                biggest = a_dictionary[x]
-                scorer = x
-        return scorer
+        if len(list_value) > 0:
+            biggest = list_value[0]
+            for x in a_dictionary:
+                if a_dictionary[x] > biggest:
+                    biggest = a_dictionary[x]
+                    scorer = x
+            return scorer
+        else:
+            return None
     else:
         return None
