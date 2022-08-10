a5f506fd77799240126b5ca60377efc9ee3ebea0
KalabSisaySE <kyimusic11@gmail.com>
2022-08-10 14:33:08 -0700

    complete task-11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""10-student module."""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves dictionary representation of the class instance.
+
+        Args:
+            attrs (list): used to filter the dictionary.
+
+        Returns:
+            dict: representation of the instance.
+        """
+        is_valid = False
+        if type(attrs) is list:
+            for i in attrs:
+                if type(i) is not str:
+                    is_valid = False
+                    break
+            is_valid = True
+        if is_valid:
+            new_dict = self.__dict__.copy()
+            for key in self.__dict__:
+                if attrs.count(key) == 0:
+                    new_dict.pop(key)
+            return new_dict
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        """reloads the instance's attributes from json.
+        
+        Args:
+            json (dict): the dictionary that contains the new information.
+        """
+        self.first_name = json['first_name']
+        self.last_name = json['last_name']
+        self.age = json['age']
