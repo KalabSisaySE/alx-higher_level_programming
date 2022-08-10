3edf6529c87d64cde6ef180e8a4ddb487383b939
KalabSisaySE <kyimusic11@gmail.com>
2022-08-10 13:36:58 -0700

    fix task-3, edit documentation

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -4,12 +4,5 @@ import json
 
 
 def to_json_string(my_obj):
-    """converts a python object to a JSON.
-
-    Args:
-        my_obj (obj): python object to be converted.
-
-    Returns:
-        str: JSON representation of an object.
-    """
+    """converts a python object to a JSON."""
     return json.dumps(my_obj)
