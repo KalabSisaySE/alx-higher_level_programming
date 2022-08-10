12257f24f72c02b164b91eba74387dc1519f5a0b
KalabSisaySE <kyimusic11@gmail.com>
2022-08-10 14:19:43 -0700

    fix task-7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,17 +5,21 @@ from sys import argv
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
+if __name__ == "__main__":
+    filename = "add_item.json"
 
-filename = "add_item.json"
-if not os.path.exists(filename):
-    save_to_json_file([], filename)
-arg_list = []
-for i in range(len(argv)):
-    arg_list.append(argv[i])
-arg_list.remove('7-add_item.py')
-if type(load_from_json_file(filename)) == list:
-    file_list = load_from_json_file(filename)
-    file_list.extend(arg_list)
-    save_to_json_file(file_list, filename)
-else:
-    save_to_json_file(arg_list, filename)
+    if not os.path.exists(filename):
+        save_to_json_file([], filename)
+
+    arg_list = []
+
+    for i in range(len(argv)):
+        arg_list.append(argv[i])
+    arg_list.remove('7-add_item.py')
+
+    if type(load_from_json_file(filename)) == list:
+        file_list = load_from_json_file(filename)
+        file_list.extend(arg_list)
+        save_to_json_file(file_list, filename)
+    else:
+        save_to_json_file(arg_list, filename)
