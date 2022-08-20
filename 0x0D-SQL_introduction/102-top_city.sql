0bdc7ecbc202bb26c2bd79e97dcfd3fc25d24ad6
KalabSisaySE <kyimusic11@gmail.com>
2022-08-20 14:16:53 -0700

    fix task-19

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,6 +1,7 @@
 -- displays top 3 of cities temp ducing july and Augus ordered by temp
-SELECT TOP 3 `city`, AVG(`value`) AS `avg_temp`
+SELECT `city`, AVG(`value`) AS `avg_temp`
 FROM `temperatures`
 WHERE `month` = 7 OR `month` = 8
 GROUP BY `city`
 ORDER BY `avg_temp` DESC
+LIMIT 3;
