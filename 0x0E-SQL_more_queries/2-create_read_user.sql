5d87b28c0734f313d07f770df44a7493b230d94c
KalabSisaySE <kyimusic11@gmail.com>
2022-08-21 05:36:14 -0700

    fix task-2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -5,9 +5,7 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
 -- create user
-CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd':
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY "user_0d_2_pwd":
 
 -- grant select access
-GRANT SELECT
-ON hbtn_0d_2.*
-TO user_0d_2@localhost;
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
