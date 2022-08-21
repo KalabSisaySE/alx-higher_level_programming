315b024742c1914e478add32773e1a98ec53c5e4
KalabSisaySE <kyimusic11@gmail.com>
2022-08-21 06:13:55 -0700

    fix task-7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -9,9 +9,9 @@ CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
 USE `hbtn_0d_usa`;
 
 -- create table
-CREATE TABLE IF NOT EXISTS `cities` (
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
     `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     `states_id` INT NOT NULL,
     `name` VARCHAR(256) NOT NULL,
-    FOREIGN KEY (`states_id`) REFERENCES `states`(`id`)
+    FOREIGN KEY (`states_id`) REFERENCES hbtn_0d_usa.states(`id`)
 );
