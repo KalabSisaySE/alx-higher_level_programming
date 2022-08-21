a2f7f9a8285ed8b1ffe8a71d5bf00ec6ece7b8ca
KalabSisaySE <kyimusic11@gmail.com>
2022-08-21 06:17:48 -0700

    fix task-7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,15 +2,8 @@
 -- `id` is unique, auto generated, not null and primary key
 -- `states_id` FK references `id` in `states` table
 
--- create database
-CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
-
--- use database
-USE `hbtn_0d_usa`;
-
--- create table
-CREATE TABLE IF NOT EXISTS `cities` (
-    `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    `states_id` INT NOT NULL FOREIGN KEY REFERENCES states(id),
-    `name` VARCHAR(256) NOT NULL
-);
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+state_id INT NOT NULL,
+name VARCHAR(256) NOT NULL,
+FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id));
