2878cc4bc054a621261f8946c5f4816f077b5676
KalabSisaySE <kyimusic11@gmail.com>
2022-08-21 08:47:27 -0700

    complete task-11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+the `11-square` module.
+defines a class `Square`.
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """represents a square based on the `Rectangle` class."""
+
+    def __init__(self, size):
+        """instantiates a square object.
+
+        Args:
+            size (int): size of the square.
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """calculates the area of the square.
+
+        Returns:
+            int: area of the square.
+        """
+        return super().area()
+
+    def __str__(self):
+        """returns a string representation of Square object."""
+        return super().__str__()
