b9ac9f516fe0b37e63dcf02d49125e297c0abb69
KalabSisaySE <kyimusic11@gmail.com>
2022-08-21 08:49:24 -0700

    complete task-13

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+the `101-add_attribute` module.
+defines a function `add_attribute`.
+"""
+
+
+def add_attribute(obj, name, value):
+    """adds attribute to `cls`.
+    
+    Args:
+        obj (obj): the object to add the attribute to.
+        name (str): name of the attribute.
+        value (any): the value of the attribute.
+    """
+    if not hasattr(obj, "__dict__"):
+        raise TypeError("can't add new attribute")
+    setattr(obj, name, value)
