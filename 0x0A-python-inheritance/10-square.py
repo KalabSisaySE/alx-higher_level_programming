3a465dbedaaec5ff961f050043c7a6b9cba59005
KalabSisaySE <kyimusic11@gmail.com>
2022-08-21 08:44:44 -0700

    fix task-10 pep8

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -5,12 +5,13 @@ defines a class `Square`.
 """
 Rectangle = __import__('9-rectangle').Rectangle
 
+
 class Square(Rectangle):
     """represents a square based on the `Rectangle` class."""
 
     def __init__(self, size):
         """instantiates a square object.
-        
+
         Args:
             size (int): size of the square.
         """
@@ -20,7 +21,7 @@ class Square(Rectangle):
 
     def area(self):
         """calculates the area of the square.
-        
+
         Returns:
             int: area of the square.
         """
