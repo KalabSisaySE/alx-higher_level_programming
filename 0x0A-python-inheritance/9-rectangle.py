78d9731291f6231c80063fd003003b32b5bdbe2f
KalabSisaySE <kyimusic11@gmail.com>
2022-08-21 08:38:30 -0700

    task-9

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """define a class `Rectangle` that inherits
 from imported class `BaseGeometry`."""
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class BaseGeometry:
