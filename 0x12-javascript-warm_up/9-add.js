020d837349ff9325d568c13394b4398496fbe21a
KalabSisaySE <kalab1275@gmail.com>
2022-09-05 19:37:02 +0000

    fix task-9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-a = parseInt(process.argv[2]);
-b = parseInt(process.argv[3]);
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);
 
 function add (a, b) {
   return a + b;
