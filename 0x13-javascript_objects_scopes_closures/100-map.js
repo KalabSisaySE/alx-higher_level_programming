8e7d9a949464950e146dbf2d9118104c81902477
KalabSisaySE <kyimusic11@gmail.com>
2022-09-06 12:41:28 -0700

    fix task-11

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -2,8 +2,7 @@
 
 const list = require('./100-data').list;
 
-const newList = list.map(function (item) {
-  return item * list.indexOf(item);
-});
+const newList = list.map((x, index) => x * index);
+
 console.log(list);
 console.log(newList);
