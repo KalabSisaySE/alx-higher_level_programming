56045c3737651b96300f80013f900320d2f4706d
KalabSisaySE <kyimusic11@gmail.com>
2022-09-06 12:32:09 -0700

    fix task-11

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 
-const list = require("./100-data").list;
+const list = require('./100-data').list;
 
-const newList = list.map(function(item) {
-    return item * list.indexOf(item);
+const newList = list.map(function (item) {
+  return item * list.indexOf(item);
 });
 console.log(list);
 console.log(newList);
+
