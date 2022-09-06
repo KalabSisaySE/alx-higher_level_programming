77eef78725183021accb0d414af17af8330bd3ad
KalabSisaySE <kyimusic11@gmail.com>
2022-09-06 12:05:30 -0700

    complete task-4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let str = '';
+      for (let j = 0; j < this.width; j++) {
+        str = str.concat('X');
+      }
+      console.log(str);
+    }
+  }
+
+  rotate () {
+    this.width = this.width + this.height;
+    this.height = this.width - this.height;
+    this.width = this.width - this.height;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+};
