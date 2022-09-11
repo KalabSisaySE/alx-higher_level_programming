5270582c1763998993dfd326506f038859cea260
KalabSisaySE <kyimusic11@gmail.com>
2022-09-11 10:51:00 -0700

    complete task-2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""the `2-my_filter_states` module
+prints lists of states with a name matches the given argument
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost",
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3],
+                         port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE '{}' ORDER BY \
+    id ASC".format(argv[4]))
+    rows = cur.fetchall()
+    for row in rows:
+        if row[1][0] == "N":
+            print(row)
+    cur.close()
+    db.close()
