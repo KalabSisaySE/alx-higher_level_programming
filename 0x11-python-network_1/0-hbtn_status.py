9cb6b335879db55c1d31234cf958c208f38537f5
KalabSisaySE <kyimusic11@gmail.com>
2022-09-23 12:53:34 -0700

    complete task-0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""the `0-htbn_status` module
+fetches a url and displays the bodu of response"""
+
+if __name__ == "__main__":
+    from urllib import request
+
+    with request.urlopen("https://alx-intranet.hbtn.io/status") as response:
+        html = response.read()
+        print('Body response:')
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode('utf-8')))
