45e73539ae98ae56b6ad4e637c4962c2b1d66a2a
KalabSisaySE <kyimusic11@gmail.com>
2022-09-23 13:20:51 -0700

    fix task-0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,11 +3,10 @@
 fetches a url and displays the bodu of response"""
 
 if __name__ == "__main__":
-    import urllib.request
-
-    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as res:
-        html = res.read()
-        print("Body response:")
+    import urllib.request as request
+    with request.urlopen('https://intranet.hbtn.io/status') as r:
+        html = r.read()
+        print('Body response:')
         print("\t- type: {}".format(type(html)))
         print("\t- content: {}".format(html))
-        print("\t- utf8 content: {}".format(html.decode("utf-8")))
+        print("\t- utf8 content: {}".format(html.decode('utf-8')))
