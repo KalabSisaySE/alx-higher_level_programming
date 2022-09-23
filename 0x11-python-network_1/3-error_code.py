bb7487429add799f79277f3b90b74d1f989aa4cf
KalabSisaySE <kyimusic11@gmail.com>
2022-09-23 12:54:24 -0700

    complete task-3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""the `3-error_code` module
+takes in a URL as an argument and
+sends a request to the URL and
+displays the body of the response (decoded in utf-8)
+"""
+
+
+if __name__ == "__main__":
+    from sys import argv
+    from urllib import request, error
+
+    try:
+        with request.urlopen(argv[1]) as response:
+            print(response.read().decode('UTF-8'))
+    except error.HTTPError as e:
+        print('Error code:', e.code)
