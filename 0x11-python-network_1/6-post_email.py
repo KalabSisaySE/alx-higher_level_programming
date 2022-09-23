c4de0895cde63cd52fff61d303daa67121158fb7
KalabSisaySE <kyimusic11@gmail.com>
2022-09-23 12:54:45 -0700

    complete task-6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""the `6-post_email` module
+takes in a URL and an email address as an argument and
+sends a POST request to the passed URL with the email as a parameter and
+displays the body of the response
+"""
+
+if __name__ == "__main__":
+    from sys import argv
+    from urllib import request
+    url = argv[1]
+
+    request = request.Request(url)
+    with request.urlopen(request) as response:
+        print(response.headers.get("X-Request-Id"))
