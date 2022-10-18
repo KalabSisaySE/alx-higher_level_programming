2cf7ed0d2e196b93681369ff86c4b102f7d6be52
KalabSisaySE <kyimusic11@gmail.com>
2022-10-19 00:30:56 +0300

    complete task-4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const request = require('request');
+const requset = require('request');
+
+// const url = process.argv[2] + "/people/18"
+const url = process.argv[2];
+request(url, (error, response, body) => {
+  if (!error && response.statusCode == 200) {
+    const results = JSON.parse(body).results;
+    const characthers = results.map(x => x.characters);
+    const wed_ant = characthers.filter(x => x.includes('https://swapi-api.hbtn.io/api/people/18/'));
+    console.log(wed_ant.length);
+  }
+});
