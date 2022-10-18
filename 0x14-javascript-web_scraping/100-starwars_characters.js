6ee1d693d0f3531bb21ad3a0ae69b8e07047c58c
KalabSisaySE <kyimusic11@gmail.com>
2022-10-19 00:31:07 +0300

    complete task-7

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+
+request(url, (error, response, body) => {
+  if (!error && response.statusCode == 200) {
+    const characters = JSON.parse(body).characters;
+    for (const character of characters) {
+      request(character, (error, response, body) => {
+        if (!error && response.statusCode == 200) {
+          console.log(JSON.parse(body).name);
+        }
+      });
+    }
+  }
+});
