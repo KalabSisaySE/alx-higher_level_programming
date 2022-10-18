c8371ad8b5a4abc0723d85161e1eb9253ec1518a
KalabSisaySE <kyimusic11@gmail.com>
2022-10-19 00:30:46 +0300

    complete task-3

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const request = require('request');
+url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`;
+
+request(url, (error, response, body) => {
+  // console.log(body);
+  console.log(JSON.parse(body).title);
+});
