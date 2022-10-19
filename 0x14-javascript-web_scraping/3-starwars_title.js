1e24b3042cbf86d9aafebc1515cd87abd947564b
KalabSisaySE <kyimusic11@gmail.com>
2022-10-19 13:35:17 +0300

    fix task-3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,9 +1,12 @@
 #!/usr/bin/node
-
 const request = require('request');
-url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`;
-
+const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`;
 request(url, (error, response, body) => {
-  // console.log(body);
-  console.log(JSON.parse(body).title);
+  if (error) {
+    console.log(error);
+  } else if (response.statusCode === 200) {
+    console.log(JSON.parse(body).title);
+  } else {
+    console.log(`An error occured. Status code: ${response.statusCode}`);
+  }
 });
