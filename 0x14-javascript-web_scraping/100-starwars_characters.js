b4421d9306bd6fcc8bc45c98ce9f9668d578d5a0
KalabSisaySE <kyimusic11@gmail.com>
2022-10-19 13:36:47 +0300

    fix task-7

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,18 +1,23 @@
 #!/usr/bin/node
-
 const request = require('request');
-
-const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-
+const url = `ttps://swapi-api.hbtn.io/api/films/'${process.argv[2]}`;
 request(url, (error, response, body) => {
-  if (!error && response.statusCode == 200) {
+  if (error) {
+    console.log(error);
+  } else if (response.statusCode === 200) {
     const characters = JSON.parse(body).characters;
     for (const character of characters) {
       request(character, (error, response, body) => {
-        if (!error && response.statusCode == 200) {
+        if (error) {
+          console.log(error);
+        } else if (response.statusCode === 200) {
           console.log(JSON.parse(body).name);
+        } else {
+          console.log(`An error occured. Status code: ${response.statusCode}`);
         }
       });
     }
+  } else {
+    console.log(`An error occured. Status code: ${response.statusCode}`);
   }
 });
